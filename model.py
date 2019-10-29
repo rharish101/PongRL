47832047d49ce8a213eedfe84dae80cea9fc8431
rharish101 <harish.rajagopals@gmail.com>
2019-10-29 12:11:01 +0530

    Changed weight initialization

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 """DQN model."""
 from tensorflow.keras import Sequential
+from tensorflow.keras.initializers import VarianceScaling
 from tensorflow.keras.layers import Conv2D, Dense, Flatten
 
 
@@ -7,10 +8,27 @@ def get_model(input_shape, output_dims):
     """Get the DQN model."""
     model = Sequential()
     model.add(
-        Conv2D(16, 8, strides=4, activation="relu", input_shape=input_shape)
+        Conv2D(
+            16,
+            8,
+            strides=4,
+            activation="relu",
+            kernel_initializer=VarianceScaling(2),
+            input_shape=input_shape,
+        )
     )
-    model.add(Conv2D(32, 4, strides=2, activation="relu"))
+    model.add(
+        Conv2D(
+            32,
+            4,
+            strides=2,
+            activation="relu",
+            kernel_initializer=VarianceScaling(2),
+        )
+    )
     model.add(Flatten())
-    model.add(Dense(256, activation="relu"))
-    model.add(Dense(output_dims))
+    model.add(
+        Dense(256, activation="relu", kernel_initializer=VarianceScaling(2))
+    )
+    model.add(Dense(output_dims, kernel_initializer=VarianceScaling(2)))
     return model
